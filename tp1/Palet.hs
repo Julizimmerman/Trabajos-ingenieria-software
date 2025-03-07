3cb0d48be2324ccba92f073afd257a817d67c334
Julizimmerman <jzimmerman@udesa.edu.com>
2025-03-07 11:12:03 -0300

    palet and route

--- tp1/Palet.hs
+++ tp1/Palet.hs
@@ -4,5 +4,10 @@ module Palet ( Palet, newP, destinationP, netP )
 data Palet = Pal String Int deriving (Eq, Show)
 
 newP :: String -> Int -> Palet   -- construye un Palet dada una ciudad de destino y un peso en toneladas
+newP city weight = Pal city weight
+
 destinationP :: Palet -> String  -- responde la ciudad destino del palet
+destinationP (Pal city _) = city -- Si no ponemos los paréntesis, el compilador piensa que Pal es una función y da error. 
+
 netP :: Palet -> Int             -- responde el peso en toneladas del palet
+netP (Pal _ weight) = weight     -- Si no ponemos los paréntesis, el compilador piensa que Pal es una función y da error.
--- tp1/Route.hs
+++ tp1/Route.hs
@@ -1,8 +1,15 @@
+
 module Route ( Route, newR, inOrderR )
   where
 
+import Data.List (elemIndex)
 data Route = Rou [ String ] deriving (Eq, Show)
 
 newR :: [ String ] -> Route                    -- construye una ruta segun una lista de ciudades
-inOrderR :: Route -> String -> String -> Bool  -- indica si la primer ciudad consultada esta antes que la segunda ciudad en la ruta
+newR cities = Rou cities 
 
+inOrderR :: Route -> String -> String -> Bool  -- indica si la primer ciudad consultada esta antes que la segunda ciudad en la ruta
+-- asumo que la lista dada en newR tiene a las ciudades en orden y me fijo cual viene primera de las dos que me pasan en esta funcion 
+inOrderR (Rou cities) city1 city2 = case (elemIndex city1 cities, elemIndex city2 cities) of
+    (Just ix, Just iy) -> ix < iy   -- True if city1 comes before city2
+    _ -> False                      -- False if either city is missing
