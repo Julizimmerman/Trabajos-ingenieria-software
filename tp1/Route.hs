e37eb443e79ef818a8461172bdd19dbc5a5d38e0
Julizimmerman <jzimmerman@udesa.edu.com>
2025-03-11 17:50:53 -0300

    Route

--- tp1/Route.hs
+++ tp1/Route.hs
@@ -10,6 +10,8 @@ newR cities = Rou cities
 
 inOrderR :: Route -> String -> String -> Bool  -- indica si la primer ciudad consultada esta antes que la segunda ciudad en la ruta
 -- asumo que la lista dada en newR tiene a las ciudades en orden y me fijo cual viene primera de las dos que me pasan en esta funcion 
+
 inOrderR (Rou cities) city1 city2 = case (elemIndex city1 cities, elemIndex city2 cities) of
     (Just ix, Just iy) -> ix < iy   -- True if city1 comes before city2
     _ -> False                      -- False if either city is missing
+
